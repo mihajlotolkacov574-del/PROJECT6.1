6354c5158ec82483261a7a736bfc8a381cf59380
mihajlotolkacov574-del <mihajlotolkacov574@gmail.com>
2026-04-21 20:53:49 +0300

    add new files

---
+++ script.js
@@ -0,0 +1,16 @@
+function filterPlaces(category) {
+  const cards = document.querySelectorAll('.card');
+
+  cards.forEach(card => {
+    if (category === 'all') {
+      card.style.display = 'block';
+    } 
+    else {
+      if (card.classList.contains(category)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    }
+  });
+}
